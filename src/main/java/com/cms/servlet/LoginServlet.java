36170e82c49b5ee15ea9878349b98c662477ad39
Durga1234-cloud <puhandurgasankarp@gmail.com>
2024-08-07 22:32:14 +0530

    update loginServlet file

--- src/main/java/com/cms/servlet/LoginServlet.java
+++ src/main/java/com/cms/servlet/LoginServlet.java
@@ -1,4 +1,26 @@
-package com.cms.servlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class LoginServlet {
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // Dummy validation logic - replace with your own database validation
+        if ("admin".equals(username) && "password".equals(password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            response.sendRedirect("dashboard.jsp");
+        } else {
+            response.sendRedirect("login.jsp?error=Invalid+username+or+password");
+        }
+    }
 }
